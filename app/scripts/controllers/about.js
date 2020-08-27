b82a2f4b5446a03a47462585908f9de6c15c94d5
unknown <raafet.dev@gmail.com>
2020-08-27 12:21:41 +0100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name raafetApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the raafetApp
+ */
+angular.module('raafetApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
